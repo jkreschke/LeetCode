90ad2fbb4683ee099a5802a72560db3e91302ab2
jkreschke <91341686+jkreschke@users.noreply.github.com>
2021-12-03 12:22:17 -0800

    56 ms

--- solutions/152. Maximum Product Subarray.py
+++ solutions/152. Maximum Product Subarray.py
@@ -1,5 +1,9 @@
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
+        '''
+        Second attempt dp, O(n) time complexity, O(1) space complexity.
+        Store the ans, and maximum, minimum product that uses previous number
+        '''
         ans = mx = mn = nums[0]
         for x in nums[1:]:
             mx, mn = max(mx*x, mn*x, x), min(mx*x, mn*x, x)
@@ -7,7 +11,9 @@ class Solution:
         return ans
     
         '''
+        first attempt
         dp solution, time complexity O(n^2), time limit exceeded.
+        space complexity O(n)
         '''
         ans, dp = nums[0], [nums[0]]
         for x in nums[1:]:
